3d5f3db1172247c4d597d6f70a9795cc28ef943c
Shane McKee <smckee6192@gmail.com>
2016-05-06 14:43:30 -0700

    Added getNextAction and removeTopAction

--- src/Robot.hs
+++ src/Robot.hs
@@ -5,9 +5,19 @@ module Robot where
   type Name = String
 --  type Robot = Action -> State -> State
 
+  -- *BASIC*
+  --let s = State [] 0 0 0
+  --let s2 = addAction MoveForward s
+  --addAction MoveBack s2
   addAction :: Action -> State -> State
   addAction a s = let newQueue = (actionQueue s) ++ [a] in
                         let updateActionQueue q = q { actionQueue = newQueue} in
                             updateActionQueue s
 
+  getNextAction :: State -> Action
+  getNextAction s = let actionList = (actionQueue s) in
+                      head actionList
 
+  removeTopAction :: State -> State
+  removeTopAction s = let updateActionQueue q = q { actionQueue = tail (actionQueue s)} in
+      updateActionQueue s
